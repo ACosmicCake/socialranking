1ec297d36215a122d09a9ee85b30c72796b8ff6a
Phan <ACosmicCake@gmail.com>
2021-08-08 16:59:56 +0700

    Revert "fix session error"
    This reverts commit a8bde0d14b9b17cc51b9a05b4b62cd070808fc91.

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from helpers import apology,convert,people
 from lists import countries,questions,answer,lowincome,lowmiddleincome,uppermiddleincome,highincome
-import lists
 
 
 
@@ -39,75 +38,72 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-
-
 
 @app.route("/")
 def index():
     """HOMEPAGE"""
-    lists.currentquestion = -1
-    lists.country = 0
-    lists.education = 0
-    lists.wealth = 0
-    lists.access = 0
-    lists.result = 0
+    session['currentquestion'] = -1
+    session['country'] = 0
+    session['education'] = 0
+    session['wealth'] = 0
+    session['access'] = 0
+    session['result'] = 0
     return render_template("index.html",)
 
 
 @app.route("/quiz",methods=["GET", "POST"])
 def quiz():
     if request.method == "POST":
-
+        
         #itterate through questions everytimr the user clicks next
-        currentquestion = lists.currentquestion
+        currentquestion = session['currentquestion']
         currentquestion = currentquestion + 1
-        lists.currentquestion = currentquestion
-
-
+        session['currentquestion'] = currentquestion
+        
+        
         # get the GNI capita of the users country
         country = request.form.get("country")
         if country in highincome:
-            lists.country = 16
+            session['country'] = 16
         if country in uppermiddleincome:
-            lists.country = 51
+            session['country'] = 51
         if country in lowmiddleincome:
-            lists.country = 91
+            session['country'] = 91
         if country in lowincome:
-            lists.country = 100
-
+            session['country'] = 100
+            
         if currentquestion == 0 or currentquestion == 1 or currentquestion == 2 or currentquestion == 3 or currentquestion == 4 or currentquestion == 5:
             if request.form.get("radio-stacked") != None:
                 questionpoint = request.form.get("radio-stacked")
-                lists.access += int(questionpoint)
+                session['access'] += int(questionpoint)
         if currentquestion == 6:
             if request.form.get("radio-stacked") != None:
-                lists.access = lists.access/5
+                session['access'] = session['access']/5
                 questionpoint = request.form.get("radio-stacked")
-                lists.access = (int(questionpoint) + lists.access)/2
+                session['access'] = (int(questionpoint) + session['access'])/2
         if currentquestion == 7:
             if request.form.get("radio-stacked") != None:
                 questionpoint = request.form.get("radio-stacked")
-                lists.education = int(questionpoint)
+                session['education'] = int(questionpoint)
         if currentquestion == 8:
             if request.form.get("radio-stacked") != None:
                 questionpoint = request.form.get("radio-stacked")
-                lists.wealth = int(questionpoint)
-                lists.result = (lists.wealth+lists.education)/2
-                print(lists.wealth,lists.education,lists.access,lists.country)
-                result = lists.result
+                session['wealth'] = int(questionpoint)
+                session['result'] = (session['wealth']+session['education'])/2
+                print(session['wealth'],session['education'],session['access'],session['country'])
+                result = session['result']
                 return render_template("result.html",result = result)
-
-
+        
+        
         return render_template("quiz.html",questions=questions, count=currentquestion, answer = answer)
-
+        
     else:
-        lists.currentquestion = -1
-        lists.country = 0
-        lists.education = 0
-        lists.wealth = 0
-        lists.access = 0
-        lists.result = 0
+        session['currentquestion'] = -1
+        session['country'] = 0
+        session['education'] = 0
+        session['wealth'] = 0
+        session['access'] = 0
+        session['result'] = 0
         return render_template("quizcountry.html",countries=countries)
 
 
@@ -134,21 +130,21 @@ def source():
     """Display the quiz"""
 
     return render_template("source.html",)
-
+    
 @app.route("/result",methods=["GET", "POST"])
 def result():
     if request.form.get("interm") != None:
         resultcheck = request.form.get("interm")
         if resultcheck == "wealth":
-            return render_template("detailedfinance.html", wealth = lists.wealth)
+            return render_template("detailedfinance.html", wealth = session['wealth'])
         if resultcheck == "access":
-            return render_template("detailedaccess.html", access = lists.access)
+            return render_template("detailedaccess.html", access = session['access'])
         if resultcheck == "education":
-            return render_template("detailededucation.html", education = lists.education)
+            return render_template("detailededucation.html", education = session['education'])
         if resultcheck == "country":
-            return render_template("detailedcountry.html", country = lists.country)
+            return render_template("detailedcountry.html", country = session['country'])
     """Display the quiz"""
-    return render_template("detailed.html",access = lists.access, wealth = lists.wealth, education = lists.education, country = lists.country)
+    return render_template("detailed.html",access = session['access'], wealth = session['wealth'], education = session['education'], country = session['country'])
 
 
 def errorhandler(e):
